978d6af60c1ba682461f48c03ce2954248d1152c
Rangken <rangken@gmail.com>
2014-07-17 16:13:29 +0900

    demonize app

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+import argparse, daemon, os
 from flask import Flask
 from flask import request
 from models import Info
@@ -7,6 +8,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
 app = Flask(__name__)
+"""
 app.config.update(dict(
     DEBUG=True,
     SECRET_KEY='development key',
@@ -14,6 +16,7 @@ app.config.update(dict(
     PASSWORD='default'
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+"""
 
 @app.route("/save",methods=['POST'])
 def save():
@@ -29,14 +32,30 @@ def hello():
 
 @app.route('/list')
 def show_entries():
-  infos_query = db_session.query(Info)
-  infos = [dict(text=info.text, description=info.description, etc=info.etc, created_at=info.created_at) for info in infos_query]
+    infos_query = db_session.query(Info)
+    infos = [dict(text=info.text, description=info.description, etc=info.etc, created_at=info.created_at) for info in infos_query]
 
-  print infos
+    print infos
 
-  return render_template('show_infos.html', infos=infos)
+    return render_template('show_infos.html', infos=infos)
 
+def main():
+    init_db()
+    app.run(host='0.0.0.0', port=3002)
 
 if __name__ == "__main__":
-    init_db()
-    app.run(port=4200)
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--daemon', action='store_true', default=False, help='Run as daemon')
+    arg_parser.add_argument('--cwd', action='store', default='/',
+                            help='Full path of the working directory to which the process should change on daemon start.')
+    arg_parser.add_argument('--uid', action='store', type=int, default=os.getuid(),
+        help='The user ID ("UID") value and group ID ("GID") value to switch the process to on daemon start.')
+    args = vars(arg_parser.parse_args())
+
+    if args['daemon']:
+        context = daemon.DaemonContext(working_directory=args['cwd'], uid=args['uid'])
+        with context:
+            main()
+    else:
+        main()
+
